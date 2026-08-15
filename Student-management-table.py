d59b405ba5e426304f8daa383d01c850414c39ac
Pavni Vaish <vaishhpavani@gmail.com>
2026-08-15 22:33:44 +0530

    Add files via upload

---
+++ Student-management-table.py
@@ -0,0 +1,12 @@
+i = [
+    [101, "Pavni", 19, "BCA", 94],
+    [102, "Ruchika", 20, "BCA", 95],
+    [103, "Avika", 18, "BCA", 86]
+]
+
+print("Roll No\tName\tAge\tCourse\tMarks")
+
+for student in i:
+    print(student[0] , "\t", student[1], "\t", student[2], "\t",
+          student[3], "\t", student[4])
+   

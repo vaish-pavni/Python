1be17d5defbc772bd7127bce25b45e9956720d7c
vaish-pavni <vaishhpavani@gmail.com>
2026-08-20 22:06:01 +0530

    With the use of slicing we can reverse or set the limit in any input.

---
+++ slicing.py
@@ -0,0 +1,12 @@
+name = 'Pavni Vaish'
+first_name = name [0:5]
+last_name = name [6:]
+full_name= name [0:11:1]
+reversed_name = name[::-1]
+print(reversed_name)
+
+website1 = 'http://google.com'
+webiste2 = 'http://wikipedia.com'
+slice = slice(7,-4)
+print(website1[slice])
+print(webiste2[slice])
